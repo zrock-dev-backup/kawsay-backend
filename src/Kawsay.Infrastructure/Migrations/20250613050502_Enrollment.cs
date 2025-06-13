5c5b267a13b0c291e4decaf7213432e903605412
Santiago C. Saavedra <zrock_git@zohomail.com>
2025-06-13 01:05:29 -0400

    chore(migrations): generate migrations

---
+++ src/Kawsay.Infrastructure/Migrations/20250613050502_Enrollment.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Enrollment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Capacity",
+                table: "Classes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Capacity",
+                table: "Classes");
+        }
+    }
+}
